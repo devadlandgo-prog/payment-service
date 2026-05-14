48d55bcb20ae110d564e1ef230161c53282dfe59
Himanshu Ray <jackofalltradesray@gmail.com>
2026-05-15 02:16:20 +0530

    Remove defaulting for production grade security and fix webhooks

--- src/main/java/com/landgo/paymentservice/dto/request/PaymentSheetRequest.java
+++ src/main/java/com/landgo/paymentservice/dto/request/PaymentSheetRequest.java
@@ -1,8 +1,8 @@
 package com.landgo.paymentservice.dto.request;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentSheetRequest {
+    @Min(value = 50, message = "Amount must be at least 50 cents")
+    @NotNull(message = "Amount is required")
     private Long amountCent;
     
+    @NotBlank(message = "Currency is required")
     private String currency;
     
     private String description;
